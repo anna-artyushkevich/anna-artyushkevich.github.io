8ac1d4de6df882b355fd96ccb8c5f96d1d522a55
Anna Artyushkevich <nurka.ann@gmail.com>
2019-11-01 21:27:45 +0300

    add build

---
+++ precache-manifest.a59fd67950b4f9c7603492c8277adcbc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb3fc1e9e119f8f7460ebcf8d3d12386",
+    "url": "/chat_webapp/index.html"
+  },
+  {
+    "revision": "0fd3fc3166edf810a6a3",
+    "url": "/chat_webapp/static/css/main.c1eb0690.chunk.css"
+  },
+  {
+    "revision": "ebc4583dff3b7ee4e551",
+    "url": "/chat_webapp/static/js/2.e6ae8d73.chunk.js"
+  },
+  {
+    "revision": "0fd3fc3166edf810a6a3",
+    "url": "/chat_webapp/static/js/main.980f9079.chunk.js"
+  },
+  {
+    "revision": "3cdd3149188fe59aa96e",
+    "url": "/chat_webapp/static/js/runtime-main.af1b5825.js"
+  }
+]);
